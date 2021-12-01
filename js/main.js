78d698423aa5667d1c211ad5633084667d9134bf
Abdurahmon <icefroghs@gmail.com>
2021-12-01 18:08:42 +0500

    GiGa
    GiGa

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    loop: true,
+    autoplay: true,
+    delay: 5000,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+
+
+
+  
+  
